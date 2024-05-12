b58bce5783fbabec5be8678de9ced0ba202e0d6a
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-12 14:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let n=document.getElementById('name');
+let a=document.getElementById('age');
+let b=document.getElementById('btn');
+let p=new Promise((resolve,reject)=>{
+	if(parseInt(a.value)>=18){
+		resolve `Welcome, ${n.value}. You can vote.`;
+	}
+	else{
+		reject `Oh sorry ${n.value}. You aren't old enough.`
+	}
+	
+});
+b.addEventListener('click',()=>{
+	const tag=document.createElement('p');
+	p.then((d)=>{
+		tag.innerHTML=`${d}`;
+	});
+	document.getElementById('form').style.display='none';
+	document.querySelector('body').appendChild(tag);
+});
